2152a3c146ceaf223f051209fa3bf97efe3eb54c
Dhita Irma <dhita.irma@gmail.com>
2020-09-18 01:07:43 +0800

    Create Post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.TextField(max_length=240)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="liked")
+
+    def __str__(self):
+        return f"{self.creator} - {self.content}"
