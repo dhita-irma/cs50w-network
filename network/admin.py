dd5d47ced426bca7ed82145db35ec7f1b481a1c3
Dhita Irma <dhita.irma@gmail.com>
2020-09-18 13:51:27 +0800

    Create Admin & Model

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username")
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "creator", "content")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
--- network/models.py
+++ network/models.py
@@ -9,7 +9,16 @@ class Post(models.Model):
     content = models.TextField(max_length=240)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     timestamp = models.DateTimeField(auto_now_add=True)
-    likes = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="liked")
+    # likes = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="liked")
 
     def __str__(self):
         return f"{self.creator} - {self.content}"
+
+    # Return JSON representation of the email
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "creator": self.creator.username,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+        }
