66fae176f9ad3cb35eb395bf0600c52671e58189
Dhita Irma <dhita.irma@gmail.com>
2020-09-22 00:29:31 +0800

    Create Like model

--- network/models.py
+++ network/models.py
@@ -5,6 +5,15 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+    def get_following_list(self):
+        """Return a list of following user objects"""
+        return [following.following_user for following in self.following.all()]
+
+    def get_followers_list(self):
+        """Return a list of followers user objects"""
+        return [follower.user for follower in self.followers.all()]
+
+
 class Post(models.Model):
     content = models.TextField(max_length=240)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
@@ -22,6 +31,20 @@ class Post(models.Model):
             "creator": self.creator.username,
             "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
         }
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_posts")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"{self.user} likes Post {self.post.id}"
+
 
 class UserFollowing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
