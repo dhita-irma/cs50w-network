79304d86eb6e6bec49dac63819bd2f22c88b7eae
Dhita Irma <dhita.irma@gmail.com>
2020-09-22 16:35:50 +0800

    Update views.py

--- network/views.py
+++ network/views.py
@@ -1,10 +1,6 @@
 import json
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import IntegrityError
-from django.forms import ModelForm
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http404
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
